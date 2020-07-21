c96d8b8d97d524d59ada3776101fc1aacc121e37
Ari Dyckovsky <aridyckovsky@gmail.com>
2020-07-20 22:48:13 -0600

    Updates

---
+++ precache-manifest.d025d2a379200e67c6b39de6ef3ebeba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "907f860fa961f9a9d8482a5d30399008",
+    "url": "/index.html"
+  },
+  {
+    "revision": "598cbc88571747019ea8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ef1622202d34bf37c53c",
+    "url": "/static/js/2.a9daa0a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a9daa0a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "598cbc88571747019ea8",
+    "url": "/static/js/main.2c69c681.chunk.js"
+  },
+  {
+    "revision": "f3649b7bce26ddc2e82b",
+    "url": "/static/js/runtime-main.18c87004.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
